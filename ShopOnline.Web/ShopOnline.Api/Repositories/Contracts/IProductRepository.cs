08d54b591391964fc20965260b89c1a428975a79
huyho09 <huy.ho@s3corp.com.vn>
2022-06-01 23:28:12 +0700

    Added Dependency Injection and Repository Pattern

---
+++ ShopOnline.Web/ShopOnline.Api/Repositories/Contracts/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetItems();
+        Task<IEnumerable<ProductCategory>> GetCategories();
+        Task<Product> GetItem(int id);
+        Task<ProductCategory> GetCategory(int id);
+    }
+}
